c5a3e7c9bf22c06c741162666ff2bdcfb3375901
Kai Tietz <ktietz@anaconda.com>
2021-08-19 11:32:44 +0200

    bump to version 9.0.3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,72 +1,134 @@
 #!/bin/bash
 
+set -x
+
 BUILD_CONFIG=Release
 
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${target_platform} =~ .*linux.* ]]; then
-  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_FIND_ROOT_PATH="${PREFIX};${BUILD_PREFIX}/${HOST}/sysroot")
-  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES:PATH="${BUILD_PREFIX}/${HOST}/sysroot/usr/include")
-  CMAKE_PLATFORM_FLAGS+=(-DVTK_USE_X:BOOL=ON)
+# Use bash "Remove Largest Suffix Pattern" to get rid of all but major version number
+PYTHON_MAJOR_VERSION=${PY_VER%%.*}
 
-  # Default to GCC <10 behavior to avoid `multiple definition' linker errors
-  CFLAGS="${CFLAGS} -fcommon"
-  CXXFLAGS="${CXXFLAGS} -fcommon"
-elif [[ ${target_platform} == osx-64 ]]; then
-  CMAKE_PLATFORM_FLAGS+=(-DVTK_USE_X:BOOL=OFF)
-  CMAKE_PLATFORM_FLAGS+=(-DVTK_USE_COCOA:BOOL=ON)
-  CMAKE_PLATFORM_FLAGS+=(-DVTK_USE_CARBON:BOOL=OFF)
+VTK_ARGS=()
+
+if [ -f "$PREFIX/lib/libOSMesa32${SHLIB_EXT}" ]; then
+    VTK_ARGS+=(
+        "-DVTK_DEFAULT_RENDER_WINDOW_OFFSCREEN:BOOL=ON"
+        "-DVTK_OPENGL_HAS_OSMESA:BOOL=ON"
+        "-DOSMESA_INCLUDE_DIR:PATH=${PREFIX}/include"
+        "-DOSMESA_LIBRARY:FILEPATH=${PREFIX}/lib/libOSMesa32${SHLIB_EXT}"
+    )
+
+    if [[ "${target_platform}" == linux-* ]]; then
+        VTK_ARGS+=(
+            "-DVTK_USE_X:BOOL=OFF"
+        )
+    elif [[ "${target_platform}" == osx-* ]]; then
+        VTK_ARGS+=(
+            "-DVTK_USE_COCOA:BOOL=OFF"
+            "-DCMAKE_OSX_SYSROOT:PATH=${CONDA_BUILD_SYSROOT}"
+        )
+    fi
+else
+    TCLTK_VERSION=`echo 'puts $tcl_version;exit 0' | tclsh`
+
+    VTK_ARGS+=(
+        "-DVTK_DEFAULT_RENDER_WINDOW_OFFSCREEN:BOOL=OFF"
+        "-DVTK_OPENGL_HAS_OSMESA:BOOL=OFF"
+        "-DVTK_USE_TK:BOOL=ON"
+        "-DTCL_INCLUDE_PATH=${PREFIX}/include"
+        "-DTK_INCLUDE_PATH=${PREFIX}/include"
+        "-DTCL_LIBRARY:FILEPATH=${PREFIX}/lib/libtcl${TCLTK_VERSION}${SHLIB_EXT}"
+        "-DTK_LIBRARY:FILEPATH=${PREFIX}/lib/libtk${TCLTK_VERSION}${SHLIB_EXT}"
+    )
+    if [[ "${target_platform}" == linux-* ]]; then
+        VTK_ARGS+=(
+            "-DVTK_USE_X:BOOL=ON"
+        )
+    elif [[ "${target_platform}" == osx-* ]]; then
+        VTK_ARGS+=(
+            "-DVTK_USE_COCOA:BOOL=ON"
+            "-DCMAKE_OSX_SYSROOT:PATH=${CONDA_BUILD_SYSROOT}"
+        )
+    fi
 fi
 
-declare -a WITH_OSMESA
-if [[ -f "${PREFIX}"/lib/libOSMesa32.so ]]; then
-  WITH_OSMESA+=(-DVTK_OPENGL_HAS_OSMESA:BOOL=ON)
-  WITH_OSMESA+=(-DOSMESA_LIBRARY="${PREFIX}/lib/libOSMesa32.so")
+if [[ "$target_platform" != "linux-ppc64le" && "$target_platform" != "osx-arm64" ]]; then
+    VTK_ARGS+=(
+        "-DVTK_MODULE_ENABLE_VTK_GUISupportQt:STRING=YES"
+        "-DVTK_MODULE_ENABLE_VTK_RenderingQt:STRING=YES"
+    )
 fi
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  (
+    mkdir build-native
+    cd build-native
+    export CC=$CC_FOR_BUILD
+    export CXX=$CXX_FOR_BUILD
+    unset CFLAGS
+    unset CXXFLAGS
+    unset CPPFLAGS
+    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+    cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$SRC_DIR/vtk-compile-tools \
+       -DCMAKE_PREFIX_PATH=$BUILD_PREFIX \
+       -DCMAKE_INSTALL_LIBDIR=lib \
+       -DVTK_BUILD_COMPILE_TOOLS_ONLY=ON ..
+    ninja -j${CPU_COUNT}
+    ninja install
+    cd ..
+  )
+  MAJ_MIN=$(echo $PKG_VERSION | rev | cut -d"." -f2- | rev)
+  CMAKE_ARGS="${CMAKE_ARGS} -DVTKCompileTools_DIR=$SRC_DIR/vtk-compile-tools/lib/cmake/vtkcompiletools-${MAJ_MIN}/"
+  CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_REQUIRE_LARGE_FILE_SUPPORT=1 -DCMAKE_REQUIRE_LARGE_FILE_SUPPORT__TRYRUN_OUTPUT="
+fi
+
+mkdir build
+cd build || exit
+
+echo "VTK_ARGS:" "${VTK_ARGS[@]}"
 
 # now we can start configuring
-cmake -H. -Bbuild -G"Ninja" \
+cmake -LAH .. -G "Ninja" ${CMAKE_ARGS} \
+    -Wno-dev \
     -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
     -DCMAKE_PREFIX_PATH:PATH="${PREFIX}" \
+    -DCMAKE_FIND_FRAMEWORK=LAST \
     -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
     -DCMAKE_INSTALL_RPATH:PATH="${PREFIX}/lib" \
-    -DCMAKE_INSTALL_LIBDIR="lib" \
-    -DBUILD_DOCUMENTATION:BOOL=OFF \
-    -DBUILD_TESTING:BOOL=OFF \
-    -DBUILD_EXAMPLES:BOOL=OFF \
+    -DCMAKE_INSTALL_LIBDIR:PATH=lib \
+    -DVTK_BUILD_DOCUMENTATION:BOOL=OFF \
+    -DVTK_BUILD_TESTING:BOOL=OFF \
+    -DVTK_BUILD_EXAMPLES:BOOL=OFF \
     -DBUILD_SHARED_LIBS:BOOL=ON \
-    -DVTK_WRAP_PYTHON:BOOL=ON \
-    -DModule_vtkPythonInterpreter:BOOL=OFF \
-    -DVTK_PYTHON_VERSION:STRING="${PY_VER}" \
-    -DVTK_INSTALL_PYTHON_MODULE_DIR:PATH="${SP_DIR}" \
+    -DVTK_LEGACY_SILENT:BOOL=OFF \
     -DVTK_HAS_FEENABLEEXCEPT:BOOL=OFF \
-    -DVTK_RENDERING_BACKEND=OpenGL2 \
-    -DModule_vtkRenderingMatplotlib=ON \
-    -DVTK_USE_SYSTEM_ZLIB:BOOL=ON \
-    -DVTK_USE_SYSTEM_FREETYPE:BOOL=ON \
-    -DVTK_USE_SYSTEM_LIBXML2:BOOL=ON \
-    -DVTK_USE_SYSTEM_PNG:BOOL=ON \
-    -DVTK_USE_SYSTEM_JPEG:BOOL=ON \
-    -DVTK_USE_SYSTEM_TIFF:BOOL=ON \
-    -DVTK_USE_SYSTEM_EXPAT:BOOL=ON \
-    -DVTK_USE_SYSTEM_HDF5:BOOL=ON \
-    -DVTK_EXTERNAL_HDF5_IS_SHARED:BOOL=ON \
-    -DVTK_USE_SYSTEM_JSONCPP:BOOL=ON \
-    -DVTK_SMP_IMPLEMENTATION_TYPE:STRING=TBB \
-    -DVTK_USE_SYSTEM_NETCDF:BOOL=ON \
-    -DVTK_USE_SYSTEM_LZ4:BOOL=ON \
-    -DVTK_USE_SYSTEM_OGG:BOOL=ON \
-    -DVTK_USE_SYSTEM_THEORA:BOOL=ON \
-    -DModule_vtkIOXdmf2:INTERNAL=ON \
-    "${SCREEN_ARGS[@]}" \
-    "${CMAKE_PLATFORM_FLAGS[@]}" \
-    "${WITH_OSMESA[@]}"
+    -DVTK_WRAP_PYTHON:BOOL=ON \
+    -DVTK_PYTHON_VERSION:STRING="${PYTHON_MAJOR_VERSION}" \
+    -DPython3_FIND_STRATEGY=LOCATION \
+    -DPython3_ROOT_DIR=${PREFIX} \
+    -DPython3_EXECUTABLE=${PREFIX}/bin/python \
+    -DVTK_MODULE_ENABLE_VTK_PythonInterpreter:STRING=NO \
+    -DVTK_MODULE_ENABLE_VTK_RenderingFreeType:STRING=YES \
+    -DVTK_MODULE_ENABLE_VTK_RenderingMatplotlib:STRING=YES \
+    -DVTK_MODULE_ENABLE_VTK_IOFFMPEG:STRING=YES \
+    -DVTK_MODULE_ENABLE_VTK_ViewsCore:STRING=YES \
+    -DVTK_MODULE_ENABLE_VTK_ViewsContext2D:STRING=YES \
+    -DVTK_MODULE_ENABLE_VTK_PythonContext2D:STRING=YES \
+    -DVTK_MODULE_ENABLE_VTK_RenderingContext2D:STRING=YES \
+    -DVTK_MODULE_ENABLE_VTK_RenderingContextOpenGL2:STRING=YES \
+    -DVTK_MODULE_ENABLE_VTK_RenderingCore:STRING=YES \
+    -DVTK_MODULE_ENABLE_VTK_RenderingOpenGL2:STRING=YES \
+    -DVTK_MODULE_ENABLE_VTK_WebGLExporter:STRING=YES \
+    -DVTK_DATA_EXCLUDE_FROM_ALL:BOOL=ON \
+    -DVTK_USE_EXTERNAL:BOOL=ON \
+    -DVTK_MODULE_USE_EXTERNAL_VTK_libharu:BOOL=OFF \
+    -DVTK_MODULE_USE_EXTERNAL_VTK_pegtl:BOOL=OFF \
+    "${VTK_ARGS[@]}"
 
 # compile & install!
-cmake --build build/ -- -j${CPU_COUNT}
-cmake --build build/ -- install
+ninja install -v
 
 # The egg-info file is necessary because some packages,
-# like mayavi, have a __requires__ in their __init__.py,
+# like mayavi, have a __requires__ in their __invtkRenderWindow::New()it__.py,
 # which means pkg_resources needs to be able to find vtk.
 # See https://setuptools.readthedocs.io/en/latest/pkg_resources.html#workingset-objects
 
