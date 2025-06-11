6a1d4e397a79ae059c7054cc0229df61807a5fcd
Serhii Kupriienko <skupriienko@anaconda.com>
2025-06-11 12:24:15 +0300

    Fix python in tests

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -10,7 +10,7 @@ pip list | findstr "vtk"
 
 REM Verify that pkg_resources can find VTK with the correct version (not "base")
 echo "Verifying VTK package metadata..."
-for /f %%i in ('%PYTHON% -c "import pkg_resources; print(pkg_resources.get_distribution('vtk').version)"') do set VTK_DETECTED_VERSION=%%i
+for /f %%i in ('python -c "import pkg_resources; print(pkg_resources.get_distribution('vtk').version)"') do set VTK_DETECTED_VERSION=%%i
 echo "pkg_resources detected VTK version: %VTK_DETECTED_VERSION%"
 if not "%VTK_DETECTED_VERSION%"=="%PKG_VERSION%" (
     echo "ERROR: pkg_resources detected wrong VTK version!"
@@ -25,4 +25,4 @@ if not exist %PREFIX%\\Library\\bin\\vtkGUISupportQt-%PKG_VERSION_MINOR%.dll exi
 if not exist %PREFIX%\\Library\\lib\\vtkRenderingQt-%PKG_VERSION_MINOR%.lib exit 1
 if not exist %PREFIX%\\Library\\bin\\vtkRenderingQt-%PKG_VERSION_MINOR%.dll exit 1
 
-%PYTHON% test_vtk.py
+python test_vtk.py
